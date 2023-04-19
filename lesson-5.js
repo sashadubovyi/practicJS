be52ab708004f5805025d842717c4b8bcdce5bd6
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-04-19 09:29:14 +0300

    js

--- lesson-5.js
+++ lesson-5.js
@@ -777,3 +777,287 @@ Examples:
 //   }
 // ]
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// const account = {
+//   owner: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['order-1', 'order-2', 'order-3'],
+//   changeDiscount(value) {
+//     this.discount = value;
+//   },
+//   showOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost;
+//     this.orders.push(order);
+//   },
+// };
+
+// account.changeDiscount(0.15);
+// console.log(account.discount); // 0.15
+
+// console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+
+// account.addOrder(5000, 'order-4');
+// console.log(account.balance); // 19000
+// console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function (itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
+
+// invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// const car = {
+//   registrationNumber: 'GA12345',
+//   brand: 'Toyota',
+// };
+
+// function displayDetails (ownerName) {
+//   console.log(
+//     `${ownerName}, this is your car: ${this.registrationNumber} ${this.brand}`
+//   );
+// }
+
+// displayDetails.call(car, 'Sofia');
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// const car = {
+//   registrationNumber: 'GA12345',
+//   brand: 'Toyota',
+// };
+
+// const displayDetails = ownerName =>{
+//   console.log(
+//     `${ownerName}, this is your car: ${this.registrationNumber} ${this.brand}`
+//   );
+// }
+
+// const fn = displayDetails.bind(car, 'Sofia')
+// fn()
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// const directRoute = function (to = 'Chop') {
+//   console.log(`${this.from} => ${to}`);
+// };
+
+// const train1 = {
+//   from: 'Kyiv',
+//   directRoute,
+// };
+// const train2 = {
+//   from: 'Lviv',
+//   directRoute,
+// };
+
+// directRoute.apply(train1, ['Vinnytsia']);
+// directRoute.call(train1);
+// train2.directRoute();
+// train1.directRoute.call(train2, 'Odessa');
+
+// const yourDirectRoute = directRoute.bind(train1);
+// yourDirectRoute();
+// yourDirectRoute('Kharkiv');
+
+// const theirDirectRoute = directRoute.bind(train2, 'Poltava');
+// theirDirectRoute();
+// theirDirectRoute('Cherkasy');
+// theirDirectRoute.call(train1);
+
+// const anotherDirectRoute = train1.directRoute;
+// console.log(anotherDirectRoute());
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// const cart = {
+//   items: [],
+
+//     getItems() {
+//         return this.items;
+//   },
+//   add(product) {},
+//   remove(productName) {},
+//   clear() {},
+//   countTotalPrice() {},
+//   increaseQuantity(productName) {},
+//   decreaseQuantity(productName) {},
+// };
+
+// const apple = { name: '🍎', price: 50 };
+// const grape = { name: '🍇', price: 70 };
+// const lemon = { name: '🍋', price: 60 };
+// const strawberry = { name: '🍓', price: 110 };
+
+// console.table(cart.getItems());
+
+// cart.add(apple);
+// cart.add(apple);
+// cart.add(lemon);
+
+// console.table(cart.getItems());
+
+// cart.remove('🍎');
+// console.table(cart.getItems());
+
+// cart.clear();
+// console.table(cart.getItems());
+
+// cart.add(apple);
+// cart.add(apple);
+// cart.add(grape);
+// cart.add(grape);
+// cart.add(grape);
+// cart.add(lemon);
+// cart.add(strawberry);
+// cart.add(strawberry);
+
+// cart.increaseQuantity('🍎');
+
+// console.table(cart.getItems());
+
+// console.log('Total: ', cart.countTotalPrice());
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// /*
+//  * Типів транзакцій всього два.
+//  * Можна покласти чи зняти гроші з рахунку.
+//  */
+// const Transaction = {
+//   DEPOSIT: 'deposit',
+//   WITHDRAW: 'withdraw',
+// };
+
+// /*
+//  * Кожна транзакція це об'єкт із властивостями: id, type та amount
+//  */
+
+// const account = {
+//   // Поточний баланс рахунку
+//   balance: 0,
+
+//   // Історія транзакцій
+//   transactions: [],
+
+//   // Останній id транзакції
+//   lastTransactionId: 0,
+
+//   /*
+//    * Метод створює та повертає об'єкт транзакції.
+//    * Приймає суму, id та тип транзакції.
+//    */
+//     createTransaction(amount, type, id) {
+//         const transaction = { amount, type, id };
+//         return transaction;
+        
+//   },
+
+//   /*
+//    * Метод, що відповідає за додавання суми до балансу.
+//    * Приймає суму транзакції.
+//    * Викликає createTransaction для створення об'єкта транзакції
+//    * після чого додає його до історії транзакцій
+//    */
+
+//     addTransaction(amount, type, id) {
+//         const newTransaction = this.createTransaction(amount, type, id);
+//         this.transactions.push(newTransaction);
+//     },
+//     deposit(amount) {
+//         this.balance += amount;
+//         this.lastTransactionId += 1;
+//         this.addTransaction(amount, 'deposit', this.lastTransactionId)
+        
+//     },
+
+  /*
+   * Метод, що відповідає за зняття суми з балансу.
+   * Приймає суму транзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його до історії транзакцій.
+   *
+   * Якщо amount більше ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливе, недостатньо коштів.
+   */
+//     withdraw(amount) {
+//         if (this.balance < amount) {
+//             console.log(`No money honney`);
+//             return;
+//         }
+//         this.balance -= amount;
+//         this.lastTransactionId += 1;
+//         const newTransaction = this.createTransaction(amount, 'withdraw', this.lastTransactionId);
+//         this.transactions.push(newTransaction);
+//   },
+
+//   /*
+//    * Метод повертає поточний баланс
+//    */
+//     getBalance() {
+//         return this.balance;
+//   },
+
+//   /*
+//    * Метод шукає та повертає об'єкт транзакції по id
+//    */
+//     getTransactionDetails(id) {
+//         return this.transactions.find((transaction) =>  transaction.id === id )
+//   },
+
+//   /*
+//    * Метод повертає кількість коштів
+//    * певного типу транзакції з усієї історії транзакцій
+//    */
+//     getTransactionTotal(type) {
+//         this.transactions.reduce((total, transaction) => {
+//           transaction.type === type ? total + transaction.amount : total, 0
+//       })
+//   },
+// };
+
+// account.deposit(100);
+// console.log(account.getBalance());
+// account.deposit(10);
+// console.log(account.getBalance());
+// account.withdraw(20);
+// console.log(account.getBalance());
+// account.withdraw(40);
+// console.log(account.getBalance());
+// console.log(account.transactions);
+
+
+// console.log('Transaction 1: ');
+// console.log(account.getTransactionDetails(1));
+// console.log('Transaction 2: ');
+// console.log(account.getTransactionDetails(2));
+// console.log('Transaction 3: ');
+// console.log(account.getTransactionDetails(3));
+// console.log('Transaction 4: ');
+// console.log(account.getTransactionDetails(4));
+
+// console.log(
+//   'Withdrawals: ' + account.getTransactionTotal(Transaction.WITHDRAW)
+// );
+// console.log('Deposits: ' + account.getTransactionTotal(Transaction.DEPOSIT));

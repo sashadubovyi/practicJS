269682b3f9010f076eb68baf5dd258cc44fc0d00
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-04-26 09:26:51 +0300

    practic

--- lesson-10.js
+++ lesson-10.js
@@ -1,33 +1,33 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // Color Palete.Динамічно відрендери 100 блоків div з класом item задопомогою функції createPaletteItems() і помісти всі ці блоки в colorPalette.Використовуй getRandomColor для назначення рандомного кольору item.При кліку на палетку кольорів зафарбовуй body у вибраний колір.
 
-// const colorPalette = document.querySelector('.color-palette');
-// colorPalette / addEventListener('click', selectColor);
+const colorPalette = document.querySelector('.color-palette');
+colorPalette / addEventListener('click', selectColor);
 
-// function selectColor(evt) {
-//     if (evt.target === evt.currentTarget) return;
+function selectColor(evt) {
+    if (evt.target === evt.currentTarget) return;
 
-//     const targetColor = evt.target.style.backgroundColor;
-//     document.body.style.backgroundColor = targetColor;
-// }
+    const targetColor = evt.target.style.backgroundColor;
+    document.body.style.backgroundColor = targetColor;
+}
 
-// function createPaletteItems() {
-//     let markup = "";
+function createPaletteItems() {
+    let markup = "";
 
-//     for (let i = 0; i < 100; i += 1) {
-//         markup += `<div class = 'item' style = 'background-color:${getRandomColor()};'></div>`;
-//     }
-//     return markup;
-// }
+    for (let i = 0; i < 100; i += 1) {
+        markup += `<div class = 'item' style = 'background-color:${getRandomColor()};'></div>`;
+    }
+    return markup;
+}
 
-// colorPalette.insertAdjacentHTML('beforeend', createPaletteItems());
+colorPalette.insertAdjacentHTML('beforeend', createPaletteItems());
 
-// function getRandomColor() {
-//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-// }
+function getRandomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
 
-// function getRandomHex() {
-//   return Math.round(Math.random() * 256)
-//     .toString(16)
-//     .padStart(2, '0');
-// }
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, '0');
+}
--- lesson-11.js
+++ lesson-11.js
@@ -1,19 +1,19 @@
 //  Викоритовуй шаблон акордеон меню з файлу accordion.html та напиши наступний функціонал: при кліку на елемент меню, розкривай блок з текстом. При повторному кліку по елементу, розкритий текст приховується. Нажимаючи на інші елементи меню, попередньо відкриті елементи не закриваються.
 
 
-// const listRef = document.querySelector(".js-accordion-list");
+const listRef = document.querySelector(".js-accordion-list");
 
-// console.dir(listRef)
+console.dir(listRef)
 
-// function handleBtnClick(event) {
-//     if (event.target.nodeName === 'BUTTON' || event.target.nodeName === 'SPAN') {
-//         // console.log('hello');
-//         const activeElement = event.target.closest('li');
-//         const panel = activeElement.querySelector('.panel')
+function handleBtnClick(event) {
+    if (event.target.nodeName === 'BUTTON' || event.target.nodeName === 'SPAN') {
+        // console.log('hello');
+        const activeElement = event.target.closest('li');
+        const panel = activeElement.querySelector('.panel')
 
-//         panel.classList.toggle('active');
-//     }
-// }
+        panel.classList.toggle('active');
+    }
+}
 
-// listRef.addEventListener("click", handleBtnClick);
+listRef.addEventListener("click", handleBtnClick);
 

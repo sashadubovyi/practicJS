ca71a1c21a39a62c97a8369cdf11113950be7249
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-04-27 14:12:31 +0300

    12

--- lesson-15.js
+++ lesson-15.js
@@ -8,28 +8,28 @@
 //   // threshold - число або масив чисел, що вказує допустимий відсоток перетину target та root
 // };
 
-// Функція зворотного виклику. Виклик callback повертає об'єкт, що містить запис про зміни, що відбулися з цільовим елементом:
-let callback = (entries, observer) => {
-  entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add('active');
-          entry.target.setAttribute('src', entry.target.dataset.src)
-          observer.unobserve(entry.target);
-    }
-  });
-};
+// // Функція зворотного виклику. Виклик callback повертає об'єкт, що містить запис про зміни, що відбулися з цільовим елементом:
+// let callback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//           entry.target.classList.add('active');
+//           entry.target.setAttribute('src', entry.target.dataset.src)
+//           observer.unobserve(entry.target);
+//     }
+//   });
+// };
 
-// Спостерігач
-let observer = new IntersectionObserver(callback);
+// // Спостерігач
+// let observer = new IntersectionObserver(callback);
 
-// Далі знаходимо цільовий елемент, за яким спостерігатиме спостерігач:
-// let target = document.querySelector('.list-item');
-const images = document.querySelectorAll('img');
-images.forEach(img => {
-    observer.observe(img);
-});
+// // Далі знаходимо цільовий елемент, за яким спостерігатиме спостерігач:
+// // let target = document.querySelector('.list-item');
+// const images = document.querySelectorAll('img');
+// images.forEach(img => {
+//     observer.observe(img);
+// });
 
 
-// Вішаємо спостерігача на той цільовий елемент який знайшли раніше
-;
+// // Вішаємо спостерігача на той цільовий елемент який знайшли раніше
+// ;
 
